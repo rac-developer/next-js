eb6d0ec66e18c07e876bb9b0115f300702c7faa7
Rodolfo Castro <soyracm@gmail.com>
2026-02-05 10:57:33 -0400

    Creacion de counterSlice, y configuracion del coutner en hooks y store

--- 02-redux-toolkit/src/redux/features/counterSlice.ts
+++ 02-redux-toolkit/src/redux/features/counterSlice.ts
@@ -1,11 +1,24 @@
 import {createSlice} from '@reduxjs/toolkit'
 
+const initialState = {
+  counter: 0
+}
+
 export const counterSlice = createSlice({
   name: "counter",
-  initialState: {
-    value:0
-  },
+  initialState,
   reducers: {
-    
+    // Funciones
+    increment: (state) => {
+      state.counter += 1
+    },
+    decrement: (state) => {
+      state.counter -= 1
+    },
   }
 })
+// Se exporta dos veces porque el counterSlice.actions se va exportar cuando sea necesario y...
+export const {increment, decrement} = counterSlice.actions
+
+// Y este por defecto, cuano no utilice el actions
+export default counterSlice.reducer
