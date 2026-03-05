fc97008b5a4bde9a1acd89d54ba1da0eb822fcd0
Rodolfo Castro <soyracm@gmail.com>
2026-03-05 15:23:45 -0400

    install button

--- 04-auth-radix/app/auth/login/page.tsx
+++ 04-auth-radix/app/auth/login/page.tsx
@@ -1,6 +1,12 @@
+import {Button} from "@radix-ui/themes"
+import {BookmarkIcon} from "@radix-ui/react-icons"
+
 function LoginPage() {
   return (
     <div>
+      <Button>
+        	<BookmarkIcon /> Login
+      </Button>
       Login Page  
     </div>
   )
